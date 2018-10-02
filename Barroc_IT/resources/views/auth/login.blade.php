9e27701be137f8893a3c7687d8bc8f4d3f5c38d4
Hugo <hrigholthw@gmail.com>
2018-10-02 11:49:48 +0200

    2.2: Update login functionality
    Tried to change the default laravel login environment to meet our desires
    
    Note: Consider trying to make the default laravel login fit in with our environment instead of changing it

--- Barroc_IT/resources/views/auth/login.blade.php
+++ Barroc_IT/resources/views/auth/login.blade.php
@@ -1,74 +1,96 @@
-@extends('layouts.app')
+    @extends('layouts.app')
 
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Login</div>
+    @section('content')
 
-                    <div class="panel-body">
-                        <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                            {{ csrf_field() }}
-                            @if(session()->has('login_error'))
-                                <div class="alert alert-success">
-                                    {{ session()->get('login_error') }}
-                                </div>
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ URL::asset('/css/app.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('/css/vw_login.css') }}">
+
+
+        <section class="row">
+            <aside class="col s8 push-s4 blue">
+
+                <form method="POST" action="{{ route('login') }}">
+                    @if(session()->has('login_error'))
+                        <div class="alert alert-success">
+                            {{ session()->get('login_error') }}
+                        </div>
+                    @endif
+
+                        <div class="input-field col s6">
+                            
+
+                            <label for="identity" class="col-md-4 control-label">First Name</label>
+
+                            <input id="identity" type="text" class="validate" name="identity">
+
+                            <!-- IDENTITY kan hieronder ook op type vatten ipv ID -->
+                            @if ($errors->has('identity'))
+                                <span class="help-block">
+                                            <strong>{{ $errors->first('identity') }}</strong>
+                                        </span>
                             @endif
-                            <div class="form-group{{ $errors->has('identity') ? ' has-error' : '' }}">
-                                <label for="identity" class="col-md-4 control-label">Email or Username</label>
 
-                                <div class="col-md-6">
-                                    <input id="identity" type="identity" class="form-control" name="identity"
-                                           value="{{ old('identity') }}" autofocus>
 
-                                    @if ($errors->has('identity'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('identity') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
+                        </div>
 
-                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Password</label>
 
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password">
+                    <div class="form-group{{ $errors->has('identity') ? ' has-error' : '' }}">
+                        <label for="identity" class="col-md-4 control-label">Username</label>
 
-                                    @if ($errors->has('password'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
+                        <div class="col-md-6">
+                            <input id="identity" type="identity" class="form-control" name="identity"
+                                   value="{{ old('identity') }}" autofocus>
 
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Login
-                                    </button>
-
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        Forgot Your Password?
-                                    </a>
-                                </div>
-                            </div>
-                        </form>
+                            @if ($errors->has('identity'))
+                                <span class="help-block">
+                                            <strong>{{ $errors->first('identity') }}</strong>
+                                        </span>
+                            @endif
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
+
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label for="password" class="col-md-4 control-label">Password</label>
+
+                        <div class="col-md-6">
+                            <input id="password" type="password" class="form-control" name="password">
+
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-8 col-md-offset-4">
+                            <button type="submit" class="btn btn-primary">
+                                Login
+                            </button>
+                        </div>
+                    </div>
+                </form>
+
+            </aside>
+
+
+
+
+            <aside class="col s4 pull-s8">
+
+                <h3>TEST TEXT</h3>
+
+            </aside>
+
+        </section>
+
+
+
+        <script type="text/javascript" src="{{ URL::asset('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js') }}"></script>
+        <script type="text/javascript" src="{{ URL::asset('https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js') }}"></script>
+        <script type="text/javascript" src="{{ URL::asset('/js/include.js') }}"></script>
+
+    @endsection
