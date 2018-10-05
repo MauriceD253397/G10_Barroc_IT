611713d22957048c69781b1c0df47bf2471afc20
Unknown <mauricederidder1@gmail.com>
2018-10-05 11:00:58 +0200

    3.4: Update html and add query
    Query to get all data
    foreach loop

--- Barroc_IT/app/Http/Controllers/SalesController.php
+++ Barroc_IT/app/Http/Controllers/SalesController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Company;
 class SalesController extends Controller
 {
 
     public function ShowSales(){
 
+        $allCompanyData = Company::all();
+
+        // Making a base query to get all data. Will optimise later if needed.
 
         return view('sales/sales',[
-            
+            'companyData' => $allCompanyData
         ]);
     }
 
