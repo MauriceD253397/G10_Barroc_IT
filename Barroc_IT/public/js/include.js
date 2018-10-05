3def8ca401e82140a5cc8caf5bca055277506993
Hugo <hrigholthw@gmail.com>
2018-10-05 09:32:03 +0200

    1.1.1 Resolve merge conflict

--- Barroc_IT/public/js/include.js
+++ Barroc_IT/public/js/include.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
     window_size = $(window).height();
-    $('.col').height(window_size);
+    $('.content-field, .menu-field').height(window_size);
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.collapsible');
+        var instances = M.Collapsible.init(elems, options);
+    });
+
+    // Or with jQuery
+
+    $(document).ready(function(){
+        $('.collapsible').collapsible();
+    });
+
+
 });
